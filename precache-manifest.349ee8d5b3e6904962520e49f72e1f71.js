9b2c9380757462634b7caf1336420a850236abba
Julian <julian@Julians-MBP-3.home>
2019-06-29 17:53:24 -0400

    Updates

---
+++ precache-manifest.349ee8d5b3e6904962520e49f72e1f71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ad32fde21807d21d051e0ef5d84153",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "a3577167bd55e93e96f0",
+    "url": "/react-memory-game/static/css/main.b01feada.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "a3577167bd55e93e96f0",
+    "url": "/react-memory-game/static/js/main.6d6d431a.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  }
+]);
